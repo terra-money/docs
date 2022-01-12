55d1ea9d7a2a7fddbbd6684832e848b9667a51ec
Jason Stallings <jason@stallin.gs>
2022-01-12 14:43:18 -0600

    style: Make top level navbar items larger than children.

--- conf.py
+++ conf.py
@@ -73,6 +73,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 myst_enable_extensions = [
     "dollarmath",
