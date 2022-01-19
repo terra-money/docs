3513ea0ff6a8d075735e1b9d2781adf8789a2dd8
Jason Stallings <jason@stallin.gs>
2022-01-19 13:32:00 -0600

    fix: Reference local images.

--- conf.py
+++ conf.py
@@ -46,6 +46,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'node_modules']
 
+# Copies files in these directories to the root of the build
+html_extra_path = ['img/']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -84,7 +86,7 @@ myst_all_links_external = True
 panels_add_bootstrap_css = True
 nitpicky = True
 
-theme_extra_footer = """<div class="logos"><a href="https://discord.gg/e29HWwC2Mz" rel="noopener" target="_blank"><img src="https://docs.terra.money/img/discord.svg" alt="link to Terra discord room"></a> <a href="https://medium.com/terra-money" rel="noopener" target="_blank"><img src="https://docs.terra.money/img/medium.svg" alt="link to Terra medium"></a> <a href="https://twitter.com/terra_money" rel="noopener" target="_blank"><img src="https://docs.terra.money/img/twitter.svg" alt="link to Terra twitter"></a> <a href="https://www.youtube.com/channel/UCoV1RXZ9ZBGcuu_PMTTlM0g" rel="noopener" target="_blank"><img src="https://docs.terra.money/img/icon_youtube.png" alt="link to Terra youtube"></a> <a href="https://t.me/terra_announcements" rel="noopener" target="_blank"><img src="https://docs.terra.money/img/icon_telegram.png" alt="link to Terra telegram room"></a></div>"""
+theme_extra_footer = """<div class="logos"><a href="https://discord.gg/e29HWwC2Mz" rel="noopener" target="_blank"><img src="/discord.svg" alt="link to Terra discord room"></a> <a href="https://medium.com/terra-money" rel="noopener" target="_blank"><img src="/medium.svg" alt="link to Terra medium"></a> <a href="https://twitter.com/terra_money" rel="noopener" target="_blank"><img src="/twitter.svg" alt="link to Terra twitter"></a> <a href="https://www.youtube.com/channel/UCoV1RXZ9ZBGcuu_PMTTlM0g" rel="noopener" target="_blank"><img src="/icon_youtube.png" alt="link to Terra youtube"></a> <a href="https://t.me/terra_announcements" rel="noopener" target="_blank"><img src="/icon_telegram.png" alt="link to Terra telegram room"></a></div>"""
 
 # Theme options
 html_theme_options = {
