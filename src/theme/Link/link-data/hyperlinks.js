1187ba1a4af51571d1c3ee9d26d3decd5733f66f
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-08-29 16:27:00 -0700

    update: hyperlink ordering

--- src/theme/Link/link-data/hyperlinks.js
+++ src/theme/Link/link-data/hyperlinks.js
@@ -1,27 +1,27 @@
 const hyperlinks = [
   {
+    href: 'https://station.terra.money/',
+    innerHTML: 'Terra Station',
+  },
+  {
     href: 'https://jobs.lever.co/terra',
     innerHTML: 'Terra Careers',
   },
   {
-    href: 'https://agora.terra.money/',
-    innerHTML: 'Terra Agora',
+    href: 'https://status.terra.money/',
+    innerHTML: 'Terra Status',
   },
   {
     href: 'https://finder.terra.money/',
     innerHTML: 'Terra Finder',
   },
   {
-    href: 'https://station.terra.money/',
-    innerHTML: 'Terra Station',
+    href: 'https://agora.terra.money/',
+    innerHTML: 'Terra Agora',
   },
   {
     href: 'https://faucet.terra.money/',
     innerHTML: 'Terra Faucet',
-  },
-  {
-    href: 'https://status.terra.money/',
-    innerHTML: 'Terra Status',
   },
 ]
 
--- src/theme/Link/link-data/social-links.js
+++ src/theme/Link/link-data/social-links.js
@@ -1,11 +1,5 @@
 const socialLinks = [
   {
-    href: 'https://twitter.com/terra_money',
-    alt: 'Terra Twitter',
-    lightSvg: '/img/icons/links/twitter.svg',
-    darkSvg: '/img/icons/dark/links/twitter.svg',
-  },
-  {
     href: 'https://medium.com/terra-money',
     alt: 'Terra Medium',
     lightSvg: '/img/icons/links/medium.svg',
@@ -18,16 +12,22 @@ const socialLinks = [
     darkSvg: '/img/icons/dark/links/discord.svg',
   },
   {
-    href: 'https://github.com/terra-money',
-    alt: 'Terra Github',
-    lightSvg: '/img/icons/links/github.svg',
-    darkSvg: '/img/icons/dark/links/github.svg',
-  },
-  {
     href: 'https://t.me/TerraNetworkLobby',
     alt: 'Terra Telegram',
     lightSvg: '/img/icons/links/telegram.svg',
     darkSvg: '/img/icons/dark/links/telegram.svg',
+  },
+  {
+    href: 'https://twitter.com/terra_money',
+    alt: 'Terra Twitter',
+    lightSvg: '/img/icons/links/twitter.svg',
+    darkSvg: '/img/icons/dark/links/twitter.svg',
+  },
+  {
+    href: 'https://github.com/terra-money',
+    alt: 'Terra Github',
+    lightSvg: '/img/icons/links/github.svg',
+    darkSvg: '/img/icons/dark/links/github.svg',
   },
 ]
 
