28db8ca3ed958021423a853f136242c9ca030c6a
evanorti <87997759+evanorti@users.noreply.github.com>
2022-09-19 19:51:23 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,7 +132,6 @@ module.exports = async function config() {
           integrity:
             'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
           crossorigin: 'anonymous',
-          displayMode: true,
         },
       ],
   };
