9a569789c27ffbeaa71dfb9a0f056b904ad9aa7b
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-10-26 15:17:46 -0700

    feat: color mode icons
    Replacing color mode icons with emojis!
    
    Co-Authored-By: Evan <87997759+evanorti@users.noreply.github.com>

---
+++ src/theme/Icon/DarkMode/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function IconDarkMode(props) {
+  return (
+    <div {...props}>🌙</div>
+  );
+}
---
+++ src/theme/Icon/LightMode/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function IconLightMode(props) {
+  return (
+    <div {...props}>🌤</div>
+  );
+}
