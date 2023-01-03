385fe073a7aabc6b8e870ff90ae110aa341e8715
plubber <er.h.gorski@gmail.com>
2023-01-03 10:32:15 -0600

    fix: terra-station -> station routing

--- src/components/Cards/card-data/home-cards.ts
+++ src/components/Cards/card-data/home-cards.ts
@@ -6,43 +6,43 @@ const homeCards: CardProps[] = [
     lightSVG: '/img/icons/buildanapp02.svg',
     darkSVG: '/img/icons/dark/buildanapp02.svg',
     header: 'Start building',
-    summary: 'Click here to learn how to build on Terra.'
+    summary: 'Click here to learn how to build on Terra.',
   },
   {
-    href: '/learn/terra-station.html',
+    href: '/learn/station.html',
     lightSVG: '/img/icons/station/station.svg',
     darkSVG: '/img/icons/dark/station.svg',
     header: 'Get started with Station',
-    summary: 'Create a wallet and stake Luna for rewards.'
+    summary: 'Create a wallet and stake Luna for rewards.',
   },
   {
     href: '/develop/which-tools.html',
     lightSVG: '/img/icons/buildanapp01.svg',
     darkSVG: '/img/icons/dark/buildanapp01.svg',
     header: 'Tool guide',
-    summary: 'Not sure which tools to use? Start here.'
+    summary: 'Not sure which tools to use? Start here.',
   },
   {
     href: '/develop/terrain/mint-an-nft.html',
     lightSVG: '/img/icons/mintnft.svg',
     darkSVG: '/img/icons/dark/mintnft.svg',
     header: 'Mint an NFT',
-    summary: 'Follow the minting tutorial to mint your own NFT.'
+    summary: 'Follow the minting tutorial to mint your own NFT.',
   },
   {
     href: '/learn/protocol.html',
     lightSVG: '/img/icons/terra.svg',
     darkSVG: '/img/icons/dark/terra.svg',
     header: 'Learn about Terra',
-    summary: 'Learn about Luna, rewards, staking, and governance.'
+    summary: 'Learn about Luna, rewards, staking, and governance.',
   },
   {
     href: '/migration-guides.html',
     lightSVG: '/img/icons/swap-1.svg',
     darkSVG: '/img/icons/dark/swap-1.svg',
     header: 'Terra migration guides',
-    summary: 'Move from Terra Classic to the new Terra chain.'
+    summary: 'Move from Terra Classic to the new Terra chain.',
   },
-]
+];
 
 export default homeCards;
--- src/components/Cards/card-data/learn-cards.ts
+++ src/components/Cards/card-data/learn-cards.ts
@@ -6,43 +6,43 @@ const learnCards: CardProps[] = [
     lightSVG: '/img/icons/terra.svg',
     darkSVG: '/img/icons/dark/terra.svg',
     header: 'What is Terra?',
-    summary: 'Learn how Terra works.'
+    summary: 'Learn how Terra works.',
   },
   {
-    href: '/learn/terra-station',
+    href: '/learn/station',
     lightSVG: '/img/icons/station/station.svg',
     darkSVG: '/img/icons/dark/station.svg',
     header: 'Get started with Station',
-    summary: 'Create a wallet and stake Luna for rewards.'
+    summary: 'Create a wallet and stake Luna for rewards.',
   },
   {
     href: '/learn/protocol/#what-is-luna',
     lightSVG: '/img/icons/luna.svg',
     darkSVG: '/img/icons/dark/luna.svg',
     header: 'What is Luna?',
-    summary: 'Learn about Luna.'
+    summary: 'Learn about Luna.',
   },
   {
     href: '/learn/protocol/#validators',
     lightSVG: '/img/icons/core/staking.svg',
     darkSVG: '/img/icons/dark/staking.svg',
     header: 'Validators and Staking',
-    summary: 'Find out about validators, consensus, staking, and rewards.'
+    summary: 'Find out about validators, consensus, staking, and rewards.',
   },
   {
     href: '/learn/fees',
     lightSVG: '/img/icons/core/feegrant.svg',
     darkSVG: '/img/icons/dark/feegrant.svg',
     header: 'How fees work',
-    summary: 'Learn how fees become staking rewards.'
+    summary: 'Learn how fees become staking rewards.',
   },
   {
     href: '/learn/glossary',
     lightSVG: '/img/icons/Glossary.svg',
     darkSVG: '/img/icons/dark/Glossary.svg',
     header: 'Glossary',
-    summary: 'Look up terms specific to Terra.'
+    summary: 'Look up terms specific to Terra.',
   },
-]
+];
 
 export default learnCards;
