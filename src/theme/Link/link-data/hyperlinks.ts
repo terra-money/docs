3297d7914304258d183fef1f4029671e74539984
evanorti <87997759+evanorti@users.noreply.github.com>
2023-02-27 14:17:58 -0800

    Update hyperlinks.ts

--- src/theme/Link/link-data/hyperlinks.ts
+++ src/theme/Link/link-data/hyperlinks.ts
@@ -5,18 +5,28 @@ const hyperlinks: HyperlinkProps[] = [
     href: 'https://station.terra.money/',
     innerHTML: 'Station',
   },
+
   {
-    href: 'https://status.terra.money/',
-    innerHTML: 'Terra Status',
-  },
-  {
-    href: 'https://finder.terra.money/',
-    innerHTML: 'Terra Finder',
+    href: 'https://jobs.lever.co/terra',
+    innerHTML: 'Careers',
   },
   {
     href: 'https://agora.terra.money/',
-    innerHTML: 'Terra Agora',
+    innerHTML: 'Forum',
+  },
+  {
+    href: 'https://www.terra.money/brand-assets',
+    innerHTML: 'Logos',
+  },
+  {
+    href: 'https://finder.terra.money/',
+    innerHTML: 'Finder',
+  },
+  {
+    href: 'https://www.terra.money/ecosystem',
+    innerHTML: 'Ecosystem',
   },
 ];
 
 export default hyperlinks;
+
