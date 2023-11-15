c8cc4bdd68dc50c6bdec53fe95f1ae2b1f7c7fbc
evanorti <ortiz@terra.money>
2023-11-15 13:43:19 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -169,7 +169,7 @@ const sidebars = {
           collapsed: true,
           items: [
             'develop/guides/initial',
-            'develop/guides/register-fee-share',
+            'develop/guides/register-feeshare',
             'develop/guides/sign-with-multisig',
             'develop/guides/start-lcd',
             'develop/vesting',
